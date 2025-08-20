e034d9dbf0fb03a2eab0605e424388bcba68ad56
Prateekkushwaha22 <kushwahaprateek362@gmail.com>
2025-08-20 19:54:50 +0530

    Add files via upload

---
+++ Assign3__task2.py
@@ -0,0 +1,12 @@
+user = float(input("Enter your number: "))
+
+from math import sqrt
+print("Square root:", sqrt(user))
+
+from math import log
+print("Logarithm:", log(user))
+
+from math import sin
+print("Sine:", sin(user))
+
+
